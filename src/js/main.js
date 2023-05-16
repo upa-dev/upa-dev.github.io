3f6b78a00871a71c3cc54818b0e9c1b62703c8d3
Manuel Castro <manuel.castro@agot.ai>
2023-05-16 11:42:53 -0300

    Add animations for mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
-  $('#hamburger').click(function (e) {
-    $('#mobile-menu').toggle()
-  })
-
   $('.nav-item, .contact-btn').click(function (e) {
     e.preventDefault();
     $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
     }, 700, 'swing');
-    $('#mobile-menu').toggle();
+  });
+
+  $('#hamburger').click(function () {
+    $('#mobile-menu').toggleClass('translate-x-0');
   });
 });
